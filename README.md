089ddd356537183abeb7dfaa41d48cd295c06a7a
Carlos Cruz <carloscruzg295@gmail.com>
2021-01-09 17:38:27 -0600

    Linear Regression

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ _Aquí se pone en práctica la lectura de datos_
 **Twitter**
 
 _Finalmente se ejecuta al función **wordcloud** y **wordcount** para analizar la información extraída_
+
+# La información la conseguí de fuentes abiertas
+**https://api.datos.gob.mx/v1/condiciones-atmosfericas**
+**https://www.inegi.org.mx/contenidos/masiva/indicadores/temas/mortalidad/mortalidad_05_tsv.zip**
--- obtener-datos.R
+++ obtener-datos.R
@@ -3,8 +3,65 @@ library(jsonlite)
 library(rjson)
 library(ggplot2)
 library(RCurl)
+library(readxl)
+library(dplyr)
 
-## LEER JSON##
+################### LEER TEXTO ###################
+crearTabla <- function() {
+   # Lectura del archivo
+   mortalidad <- read_excel("mortalidad_05.xlsx")
+   # De toda la información tomamos únicamente la de Saltillo
+   data_mortalidad <- mortalidad %>% filter(desc_municipio == "Saltillo")
+   # La información que se requiere está en las columnas, así que las tomamos
+   years <- data.frame(names(data_mortalidad[,7:32]))
+   names(years)[1] <- "years"
+   # Obtendremos las muertes generales, de las columnas correspondientes a los años
+   deaths <- data.frame(data_mortalidad[1, 7:32])
+
+   # Crearemos un arreglo de las muertes
+   n_death <- 0
+   for (i in 1:length(deaths)) {
+      n_death[i] = deaths[[i]]
+   }
+   n_death <- data.frame(n_death)
+
+   # Unimos ambos dataframe, de esta forma tenemos una columna años y le corresponde
+   # una columna con la cantidad de muertes por año
+   tablaDatos <- data.frame(cbind(years, n_death))
+
+   # Se convierten a númerico los valores del data.frame para poder aplicar el modelo
+   # de regresión lineal simple
+   tablaDatos[] <- lapply(tablaDatos, function(x) as.numeric(as.character(x)))
+
+   return (tablaDatos)
+}
+
+# Funcion que crea el modelo de regresión lineal simple
+crearModelo <- function() {
+   modelo <- lm(n_death ~ years, data = tablaDatos2)
+
+   return (modelo)
+}
+
+# Función para generar la gráfica de regresión lineal simple
+graficarMortalidad <- function(data, predict) {                                                                              qplot(x = years, y = n_death, data = data,
+   main = "Muertes en Saltillo por año", ylab = "Muertes generales",
+   xlab = "Años", geom = c("point"),
+   method = "lm") + geom_line(aes(y = deathp), lwd = 1.2, color = 4) +
+   theme_minimal()
+}
+
+#Valores Predichos
+data_mortalidad <- crearTabla()
+modelo <- crearModelo()
+summary(modelo)
+# Predicción del modelo
+deathp <- predict(modelo)
+graficarMortalidad(data_mortalidad, deathp)
+
+################### FIN TEXTO ####################
+
+################### LEER JSON ####################
 leerApi <- function(url) {
    flujoDatos <- 0
 
@@ -34,7 +91,7 @@ leerApi <- function(url) {
    return (tablaDatos)
 }
 
-# Creación de gráfica
+# Creación de gráfica (Análisis discriminante)
 graficaCondicion <- function(tablaDatos) {
    ggplot(data, aes(x = temperaturas, fill = estados)) +
    geom_bar(position = 'identity', alpha = 0.5) +
@@ -42,5 +99,7 @@ graficaCondicion <- function(tablaDatos) {
 }
 # Enlace donde obtendremos la api
 url <- "https://api.datos.gob.mx/v1/condiciones-atmosfericas"
-data <- leerApi(url)
+data_temperaturas <- leerApi(url)
 graficaCondicion(data)
+
+################### FIN JSON #####################
